e609f129abc03949c93dd562cb250a3e8edf7afb
Maksym Lvov <leva880@ukr.net>
2021-11-02 15:28:14 +0300

    maked the random color function works

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startButton = document.querySelector('button[data-start]');
+const endButton = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+startButton.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        onStartBtnClick();
+    }, 1000);
+});
+
+
+// startBtn.addEventListener("click", () => {
+//   timerId = setInterval(() => {
+//     console.log(`I love async JS!  ${Math.random()}`);
+//   }, 1000);
+// });
+
+
+function onStartBtnClick() {
+    body.style.backgroundColor = getRandomHexColor();
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// console.log(body);
+
+
+// Напиши скрипт, который после нажатия кнопки «Start», раз в секунду меняет цвет фона <body> 
+// на случайное значение используя инлайн стиль.
+//  При нажатии на кнопку «Stop», изменение цвета фона должно останавливаться.
