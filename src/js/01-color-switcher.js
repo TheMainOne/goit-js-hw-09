d96c3a04a227c925a50d9f95f3fc87a1a97ee887
Maksym Lvov <leva880@ukr.net>
2021-11-02 15:46:57 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,6 +3,8 @@ const endButton = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 let timerId = null;
 
+endButton.setAttribute('disabled', 'disabled');
+
 startButton.addEventListener('click', () => {
     timerId = setInterval(() => {
         onStartBtnClick();
@@ -12,18 +14,22 @@ startButton.addEventListener('click', () => {
 endButton.addEventListener('click', onEndBtnClick)
 
 function onStartBtnClick() {
+    endButton.removeAttribute('disabled');
+    startButton.setAttribute('disabled', 'disabled');
     body.style.backgroundColor = getRandomHexColor();
+    
 }
 
 function onEndBtnClick() {
     clearInterval(timerId);
 }
 
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+// ⚠️ Учти, на кнопку «Start» можно нажать бесконечное количество раз.
+//  Сделай так, чтобы пока изменение темы запушено, кнопка «Start» была не активна(disabled).
 
 // Напиши скрипт, который после нажатия кнопки «Start», раз в секунду меняет цвет фона <body> 
 // на случайное значение используя инлайн стиль.
