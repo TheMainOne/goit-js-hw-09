23c69bb0a5f6079dc8d704338a4f8b4375feaa5f
Maksym Lvov <leva880@ukr.net>
2021-11-04 14:08:11 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,7 +10,7 @@ const options = {
   onClose(selectedDates) {
     const choosenTime = selectedDates[0].getTime();
     const currentTime = Date.now();
-    
+
     if (choosenTime < currentTime) {
       window.alert('Please choose a date in the future')
     } else {
@@ -27,23 +27,6 @@ const button = document.querySelector('button[data-start]');
 
 button.setAttribute('disabled', 'disabled');
 
-// function convertMs(ms) {
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   const days = pad(Math.floor(ms / day));
-//   const hours = pad(Math.floor((ms % day) / hour));
-//   const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-//   const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
-
-//   return { days, hours, minutes, seconds };
-// }
-
-// function pad(value) {
-//   return String(value).padStart(2 ,'0')
-// }
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
