f7b529fee9f3e7935eca841dd361e139ae2c071d
Maksym Lvov <leva880@ukr.net>
2021-11-05 13:48:31 +0300

    started to work with task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,16 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  Notify.failure('Please choose a date in the future')
+});
+
+
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
